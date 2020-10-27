bc92cc41d0c1d4756fa8bc25e5414412cccfd909
Boyul Kim <boyulkim1@gmail.com>
2020-10-27 02:08:59 -0700

    listens for submit events on the form

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 var $avatarURL = document.querySelector('#avatar');
 var $imageSource = document.querySelector('.imageSource');
+var $save = document.querySelector('.button');
 
 function avatarFunction(event) {
   $imageSource.setAttribute('src', event.target.value);
 }
 $avatarURL.addEventListener('blur', avatarFunction);
+
+function saveFunction(event) {
+  // console.log('test');
+}
+$save.addEventListener('click', saveFunction);
