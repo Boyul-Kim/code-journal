f5bc045900f20d7bb73760de15840b50b7facd31
Boyul Kim <boyulkim1@gmail.com>
2020-10-28 17:01:37 -0700

    corrected change requests

--- js/main.js
+++ js/main.js
@@ -123,10 +123,23 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 });
 
-document.addEventListener('click', function (event) {
+/* document.addEventListener('click', function (event) {
   if (event.target.attributes[0].value === '#' && event.target.attributes[1].value === 'edit-profile') {
     switchDataView('edit-profile');
+    console.log(event.target.tagName);
   } else if (event.target.attributes[0].value === '#' && event.target.attributes[1].value === 'profile' && data.profile.username !== '') {
     switchDataView('profile');
+  }
+}); */
+
+document.addEventListener('click', function () {
+  if (event.target.tagName !== 'A') {
+    return undefined;
+  } else {
+    if (event.target.attributes[1].value === 'edit-profile') {
+      switchDataView('edit-profile');
+    } else if (event.target.attributes[1].value === 'profile' && data.profile.username !== '') {
+      switchDataView('profile');
+    }
   }
 });
